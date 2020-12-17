0d80864b064bbbda64d000744203d82b4882b4b9
Hunter Sides <hunter.s.1337@gmail.com>
2020-12-17 12:21:47 -0600

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+    has_many  :games
+    
+  
+    has_secure_password
+  
+    def slug
+      username.downcase.gsub(" ","-")
+    end
+  
+    def self.find_by_slug(slug)
+      User.all.find{|user| user.slug == slug}
+    end
+  
+  end
